5fb04f7b29a740c18f1ecad81c1844c47d058b7f
rtmvnv <rtmvnv@yandex.ru>
2018-02-08 14:27:15 +0300

    Completed automatic sprite generation.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,15 +9,15 @@ gulp.task('watch', function() {
     server: {
       baseDir: "app"
     }
-  })
+  });
 
   watch('./app/index.html', function() {
     browserSync.reload();
-  })
+  });
 
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
-  })
+  });
 });
 
 gulp.task('cssInject', ['styles'], function() {
